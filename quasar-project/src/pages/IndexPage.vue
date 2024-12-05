b6a50f0155da5746e787a5a8018e76f3a60eef73
filip2330 <filip.stefanac2012@gmail.com>
2024-12-05 13:40:15 +0100

    Commit

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-btn label="Klikni me!" color="primary" />
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
